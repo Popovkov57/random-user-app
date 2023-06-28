01d3b4c2f9a491e4056c1543b0e493af76cde597
Popovkov57 <kevin.patrzek@live.fr>
2023-06-28 13:26:10 +0200

    Update User Card CSS

--- src/components/User.js
+++ src/components/User.js
@@ -3,20 +3,26 @@ import React, { Component } from 'react'
 export default class User extends Component {
     render() {
         return (
-            <figure className="md:flex rounded-xl p-4 bg-white ring-1 ring-slate-900/5">
-                <img className="w-32 h-32 rounded-full ring-1 ring-slate-900/5" src={this.props.user.avatar} alt="" width="384" height="512" />
-                <div className="p-4 md:text-left">
-                    <figcaption className="font-medium">
-                        <div className="text-slate-700">
-                            {this.props.user.first_name} {this.props.user.last_name}
-                        </div>
-                        <div className="text-slate-400">
-                            {this.props.user.email}
-                        </div>
-                        <p class="p-1 mt-4 bg-slate-100 font-bold rounded-lg text-center">{this.props.user.employment.title}</p>
-                    </figcaption>
+            <div className="flex flex-row rounded-xl p-4 bg-white ring-1 ring-slate-900/5 hover:bg-slate-200 cursor-pointer">
+                <div className="basis-1/4">
+                    <img className="w-32 h-32 rounded-full ring-1 ring-slate-900/5 bg-white" src={this.props.user.avatar} alt="" width="384" height="512" />
                 </div>
-            </figure>
+                <div className="basis-3/4 p-4">
+                    <div className="flex flex-col text-left">
+                        <div>
+                            <p className="text-slate-600 font-medium">
+                                {this.props.user.first_name} {this.props.user.last_name}
+                            </p>
+                            <p className="text-slate-400 font-medium">
+                                {this.props.user.email}
+                            </p>
+                        </div>
+                        <div className="mt-10 text-right">
+                            <span class="p-1 text-slate-600 bg-slate-100 font-bold rounded-lg text-center font-medium ">{this.props.user.employment.title}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
