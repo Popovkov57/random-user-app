f426dabbc8d13cd681736706543e82a0b6f51114
Popovkov57 <kevin.patrzek@live.fr>
2023-07-04 17:57:11 +0200

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import UserList from './components/UserList';
 import UserDetails from './components/UserDetails';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App p-10">
-      <Routes>
-        <Route path='/users' element={<UserList/>}></Route>
-        <Route path='/users/:userId' element={<UserDetails />}></Route>
-      </Routes>
-    </div>
+    <>
+      <NavBar />
+      <div className="App p-10">
+        <Routes>
+          <Route path='/users' element={<UserList />}></Route>
+          <Route path='/users/:userId' element={<UserDetails />}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+                <div className="flex items-center flex-shrink-0 text-white mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+                    <span className="font-semibold text-xl tracking-tight ml-2">Random User App</span>
+                </div>
+                <div className="block lg:hidden">
+                    <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                    </button>
+                </div>
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div className="text-lg lg:flex-grow">
+                        <Link to="/users" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            Users
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
