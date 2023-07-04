616fc8739f1de0b2d6ccfcca5e59012e1015b998
Popovkov57 <kevin.patrzek@live.fr>
2023-07-04 17:56:37 +0200

    Delete title user list and user details components

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -6,7 +6,6 @@ export default class UserList extends Component {
     render() {
         return (
             <>
-                <h1 className='text-gray-700 text-4xl text-center'>Users</h1>
                 <div className="mt-10 grid grid-cols-4 gap-6">
                     {
                         users.map(user => {
