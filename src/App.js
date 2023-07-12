5a82578fef882e5d68d23eb90b6eb31ce93b3804
JesusC-R <cuevasrodartej@gmail.com>
2023-07-11 20:57:15 -0700

    Refactor: Improved readability and performance

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import UserList from './components/UserList';
-import UserDetails from './components/UserDetails';
-import NavBar from './components/NavBar';
-import { Component } from 'react';
-import UserEdit from './components/UserEdit';
-import UserCreation from './components/UserCreation';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import UserList from "./components/UserList";
+import UserDetails from "./components/UserDetails";
+import NavBar from "./components/NavBar";
+import { Component } from "react";
+import UserEdit from "./components/UserEdit";
+import UserCreation from "./components/UserCreation";
 
 class App extends Component {
-
   render() {
     return (
-      <>
+      <Router>
         <NavBar />
         <div className="App p-10 bg-slate-100">
-          <Routes>
-            <Route path='/users' element={<UserList />}></Route>
-            <Route path='/users/details/:userId' element={<UserDetails />}></Route>
-            <Route path='/users/edit/:userId' element={<UserEdit />}></Route>
-            <Route path='/users/new' element={<UserCreation />}></Route>
-          </Routes>
+          <Switch>
+            <Route path="/users" exact component={UserList} />
+            <Route path="/users/details/:userId" component={UserDetails} />
+            <Route path="/users/edit/:userId" component={UserEdit} />
+            <Route path="/users/new" component={UserCreation} />
+          </Switch>
         </div>
-      </>
-    )
+      </Router>
+    );
   }
 }
 
--- src/base.js
+++ src/base.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-
-// Update with your firebase config 
+// Update with your firebase config
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
   databaseURL: "",
   projectId: "",
-}
+};
 
 const app = initializeApp(firebaseConfig);
 export const base = getDatabase(app);
 
 // this is a default export
-export default base
+export default base;
