f284ebbcfcd1af5c406e97b763d9dd01a232be49
Popovkov57 <kevin.patrzek@live.fr>
2023-07-12 21:21:24 +0200

    Create .env file

--- src/base.js
+++ src/base.js
@@ -4,10 +4,10 @@ import { getDatabase } from "firebase/database";
 
 // Update with your firebase config 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID
 }
 
 const app = initializeApp(firebaseConfig);
