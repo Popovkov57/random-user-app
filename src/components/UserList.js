159064faeecaab3aa30231a8676e7e2f9bdb3c45
Popovkov57 <kevin.patrzek@live.fr>
2023-07-14 16:02:20 +0200

    Update user list component with mixin

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -55,7 +55,7 @@ const UserList = () => {
                     </div>
                 </button>
             </div>
-            <div className="mt-5 grid grid-cols-4 gap-6">
+            <div className="mt-5 grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 gap-6 md:grid-cols-2 sm:grid-cols-1">
                 {
                     loader ?
                         <div className="mb-12">
