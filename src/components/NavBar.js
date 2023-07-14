1ba8a5988e1f551bfb922ab09d971ae8acec1081
Popovkov57 <kevin.patrzek@live.fr>
2023-07-14 16:18:50 +0200

    Hidde link in responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,9 +16,9 @@ export default class NavBar extends Component {
                         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                     </button>
                 </div>
-                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto hidden">
                     <div className="text-lg lg:flex-grow">
-                        <Link to="/users" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        <Link to="/users" className="block mt-4 lg:mt-0 text-teal-200 hover:text-white mr-4">
                             Users
                         </Link>
                     </div>
