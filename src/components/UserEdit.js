f6d01a7182252217f645a3ede6915c3ee81b1a2b
Popovkov57 <kevin.patrzek@live.fr>
2023-07-15 00:48:06 +0200

    Update Style of user edit component for mobile

--- src/components/UserEdit.js
+++ src/components/UserEdit.js
@@ -86,15 +86,15 @@ const UserEdit = () => {
     return (
         <>
             <div className="flex flex-row justify-center">
-                <div className="mt-5 ml-5 mr-5 flex flex-row rounded-xl p-2 bg-white ring-1 ring-slate-900/5 basis-3/5 text-center">
-                    <div className="basis-1/4 p-4">
+                <div className="mt-5 ml-5 mr-5 flex flex-row max-sm:flex-wrap rounded-xl p-2 bg-white ring-1 ring-slate-900/5 basis-3/5 text-center">
+                    <div className="sm:basis-1/4 p-4">
                         <img className="rounded-full ring-1 ring-slate-900/5 bg-white" src={user?.avatar} alt="" width="384" height="512" />
                     </div>
-                    <div className="basis-3/4 p-4 ml-5 mt-5 mr-5">
+                    <div className="sm:basis-3/4 p-4 ml-5 mt-5 mr-5">
                         <div className="flex flex-col text-left">
                             <form className="w-full">
                                 <div className="flex flex-wrap -mx-3 mb-3">
-                                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                    <div className="w-full md:w-1/2 px-3 sm:mb-6 md:mb-0">
                                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                             First Name
                                         </label>
@@ -125,7 +125,7 @@ const UserEdit = () => {
                                             Employment Title
                                         </label>
                                         <input
-                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 sm:mb-3 leading-tight focus:outline-none focus:bg-white"
                                             name="employment.title"
                                             type="text"
                                             placeholder="Developer"
@@ -139,7 +139,7 @@ const UserEdit = () => {
                                             Email
                                         </label>
                                         <input
-                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 sm:mb-3 leading-tight focus:outline-none focus:bg-white"
                                             name="email"
                                             type="email"
                                             placeholder="jane@doe.com"
@@ -153,7 +153,7 @@ const UserEdit = () => {
                                             Phone Number
                                         </label>
                                         <input
-                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 sm:mb-3 leading-tight focus:outline-none focus:bg-white"
                                             name="phone_number"
                                             type="text"
                                             placeholder="+7-6 760-610-4694"
@@ -167,7 +167,7 @@ const UserEdit = () => {
                                             Address
                                         </label>
                                         <input
-                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 sm:mb-3 leading-tight focus:outline-none focus:bg-white"
                                             name="address.street_address"
                                             type="email"
                                             placeholder="9199 Will Drives"
@@ -176,7 +176,7 @@ const UserEdit = () => {
                                     </div>
                                 </div>
                                 <div className="flex flex-wrap -mx-3 mb-2">
-                                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                    <div className="w-full md:w-1/3 px-3 sm:mb-6 mb-3 md:mb-0">
                                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                             City
                                         </label>
@@ -214,7 +214,7 @@ const UserEdit = () => {
                                     </div>
                                 </div>
                             </form>
-                            <div className="flex flex-row mt-6 justify-end">
+                            <div className="flex flex-row mt-6 sm:justify-end justify-center">
                                 <button className='flex flex-row bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-4 text-white py-1 px-2 rounded' onClick={updateUser}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-1">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
